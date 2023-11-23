87877aa9b587ec9943deb9090d5784d074990f77
Vladyslav G <hello@mikield.rocks>
2023-11-23 17:08:19 +0100

    Get

--- src/webhooks.ts
+++ src/webhooks.ts
@@ -45,6 +45,21 @@ export class API {
     });
   }
 
+  @Get("/")
+  indexGet(context: Context): void {
+     context.body = `
+      <div style="text-align: center">
+        <h1>
+          <a href="https://discordx.js.org">discord.ts</a> rest api server example
+        </h1>
+        <p>
+          powered by <a href="https://koajs.com/">koa</a> and
+          <a href="https://www.npmjs.com/package/@discordx/koa">@discordx/koa</a>
+        </p>
+      </div>
+    `;
+  }
+
   @Post("/")
   async index(context: Context): Promise<void> {
     const body = context.request.body as ChannelPostUpdate;
